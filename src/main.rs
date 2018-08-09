9af1c061cb196c9aa0eb4c4d142d73b4b68dab68
Santiago Pastorino <spastorino@gmail.com>
2018-08-09 17:49:24 -0300

    Extract the command and its arguments from the input line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::io::{self, Write};
 use std::process::Command;
 
+// Extract the command and its arguments from the commandline
+fn get_command(line: String) -> (String, Vec<String>) {
+    let command: String = line.split_whitespace().take(1).collect();
+    let args: Vec<String> = line.split_whitespace().skip(1).map(String::from).collect();
+    (command, args)
+}
+
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
@@ -10,8 +17,7 @@ fn main() -> io::Result<()> {
         print!("> ");
         stdout.flush()?;
         stdin.read_line(&mut line)?;
-        let command: String = line.split_whitespace().take(1).collect();
-        let args: Vec<_> = line.split_whitespace().skip(1).collect();
+        let (command, args) = get_command(line);
         let output = Command::new(command).args(&args).output()?;
         print!("{}", String::from_utf8_lossy(&output.stdout));
     }
