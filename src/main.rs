e06181914251c90f2045846e803660f61f76a3ac
Santiago Pastorino <spastorino@gmail.com>
2018-09-20 16:05:11 -0300

    Add rustyline for input handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![feature(uniform_paths)]
 #![feature(try_from)]
 
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 use std::convert::TryFrom;
-use std::io::{self, Write};
 use std::process::{Command, Stdio};
 
 mod builtin;
@@ -13,72 +15,74 @@ use cmd::{Statement, Statements};
 use error::Error;
 
 fn main() -> Result<(), Error> {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
+    let mut rl = Editor::<()>::new();
 
     loop {
-        let mut line = String::new();
-        print!("> ");
-        stdout.flush()?;
-        stdin.read_line(&mut line)?;
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_ref());
+                match handle(line) {
+                    Ok(()) => {}
+                    Err(Error::NoBinary) => {}
+                    Err(e) => eprintln!("rush: {:?}", e),
+                }
+            }
+            Err(ReadlineError::Interrupted) => {}
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    Ok(())
+}
 
-        match Statements::try_from(line.as_ref()) {
-            Ok(statements) => {
-                for statement in statements {
-                    match statement {
-                        Statement::Cmd { binary, args } => {
-                            if binary == "exit" {
-                                builtin::exit(args);
-                            } else {
-                                match Command::new(binary).args(args).output() {
-                                    Ok(output) => {
-                                        print!("{}", String::from_utf8_lossy(&output.stdout))
-                                    }
-                                    Err(e) => eprintln!("rush: {:?}", e),
-                                }
-                            }
-                        }
-
-                        Statement::Pipe { input, output } => {
-                            let input = *input;
-                            let output = *output;
-
-                            match (input, output) {
-                                (
-                                    Statement::Cmd {
-                                        binary: binary_in,
-                                        args: args_in,
-                                    },
-                                    Statement::Cmd {
-                                        binary: binary_out,
-                                        args: args_out,
-                                    },
-                                ) => {
-                                    let first_child = Command::new(binary_in)
-                                        .args(args_in)
-                                        .stdout(Stdio::piped())
-                                        .spawn()?;
-                                    let first_out =
-                                        first_child.stdout.ok_or_else(|| Error::Spawn)?;
-                                    let second_child = Command::new(binary_out)
-                                        .args(args_out)
-                                        .stdin(Stdio::from(first_out))
-                                        .stdout(Stdio::piped())
-                                        .spawn()?;
-                                    let output = second_child.wait_with_output()?;
-                                    print!("{}", String::from_utf8_lossy(&output.stdout));
-                                }
-
-                                _ => unimplemented!(),
-                            }
-                        }
+fn handle(line: String) -> Result<(), Error> {
+    let statements = Statements::try_from(line.as_ref())?;
+    for statement in statements {
+        match statement {
+            Statement::Cmd { binary, args } => {
+                if binary == "exit" {
+                    builtin::exit(args);
+                } else {
+                    match Command::new(binary).args(args).output() {
+                        Ok(output) => print!("{}", String::from_utf8_lossy(&output.stdout)),
+                        Err(e) => eprintln!("rush: {:?}", e),
                     }
                 }
             }
 
-            Err(Error::NoBinary) => {}
-
-            Err(e) => eprintln!("rush: {:?}", e),
+            Statement::Pipe { input, output } => match (*input, *output) {
+                (
+                    Statement::Cmd {
+                        binary: binary_in,
+                        args: args_in,
+                    },
+                    Statement::Cmd {
+                        binary: binary_out,
+                        args: args_out,
+                    },
+                ) => {
+                    let first_child = Command::new(binary_in)
+                        .args(args_in)
+                        .stdout(Stdio::piped())
+                        .spawn()?;
+                    let first_out = first_child.stdout.ok_or_else(|| Error::Spawn)?;
+                    let second_child = Command::new(binary_out)
+                        .args(args_out)
+                        .stdin(Stdio::from(first_out))
+                        .stdout(Stdio::piped())
+                        .spawn()?;
+                    let output = second_child.wait_with_output()?;
+                    print!("{}", String::from_utf8_lossy(&output.stdout));
+                }
+                _ => unimplemented!(),
+            },
         }
     }
+    Ok(())
 }
