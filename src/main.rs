40e6c087807b687645dc994610df49c36f010bc3
Santiago Pastorino <spastorino@gmail.com>
2018-10-11 16:47:12 -0300

    Initial REPL

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use std::io;
+use std::io::{self, Write};
+use std::process::Command;
 
 fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
     loop {
-        // Read line from standard input
-
-        // Parse line into executable command
-
-        // Execute the command in a separate process
-
-        // Show output
+        let mut line = String::new();
+        print!("> ");
+        stdout.flush()?;
+        stdin.read_line(&mut line)?;
+        let command: String = line.split_whitespace().take(1).collect();
+        let args: Vec<_> = line.split_whitespace().skip(1).collect();
+        let output = Command::new(command).args(&args).output()?;
+        print!("{}", String::from_utf8_lossy(&output.stdout));
     }
 }
