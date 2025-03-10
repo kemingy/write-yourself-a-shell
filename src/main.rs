0cd93674f032f6256c35da5261b214892beda099
Keming <kemingy94@gmail.com>
2025-03-10 16:42:47 +0800

    block 1
    Signed-off-by: Keming <kemingy94@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,62 @@
-use std::io;
+use std::io::{self, IsTerminal, Write};
+use std::process::Command;
+
+static PROMPT: &[u8] = ">> ".as_bytes();
+static EXIT: &str = "exit";
+
+#[derive(Debug)]
+struct Cmd {
+    program: String,
+    args: Vec<String>,
+}
+
+impl Cmd {
+    fn from_string(text: &str) -> Self {
+        let mut chunks = text.split_whitespace();
+        Self {
+            program: chunks.next().unwrap().to_string(),
+            args: chunks.map(|s| s.to_string()).collect(),
+        }
+    }
+
+    pub fn run(&self) -> io::Result<()> {
+        match Command::new(&self.program).args(&self.args).spawn() {
+            Ok(mut child) => {
+                child.wait()?;
+            }
+            Err(e) => {
+                eprintln!("{:?}", e);
+            }
+        }
+        Ok(())
+    }
+}
+
+fn show_prompt() -> io::Result<()> {
+    let mut output = io::stdout();
+    if output.is_terminal() {
+        output.write(PROMPT)?;
+        output.flush()?;
+    }
+    Ok(())
+}
+
+fn read_stdin() -> io::Result<String> {
+    let mut buf = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut buf)?;
+    Ok(buf)
+}
+
 fn main() -> io::Result<()> {
     loop {
-        // Read line from standard input
-        // Parse line into executable command
-        // Execute the command in a separate process
-        // Show output
+        show_prompt()?;
+        let line = read_stdin()?;
+        if line.trim() == EXIT {
+            break;
+        }
+        let cmd = Cmd::from_string(&line);
+        cmd.run()?;
     }
+    Ok(())
 }
